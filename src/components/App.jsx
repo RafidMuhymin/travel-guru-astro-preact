d0d66d66656bb20939815901c861aceb72275466
RafidMuhymin <rafidmuhymin@gmail.com>
2021-08-13 10:58:39 -0700

    improved spafy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,64 @@
 import { h } from "preact";
 
 export default function App() {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      entry.isIntersecting && createPrefetchLink(entry.target);
-    });
-  });
-
-  document.querySelectorAll('a[href^="/"').forEach((link) => {
-    link.addEventListener("mouseover", () => {
-      createPrefetchLink(link);
-    });
-
-    link.addEventListener("click", (e) => {
-      if (!window.event.ctrlKey) {
-        e.preventDefault();
-        fetchHTML(link.href);
-      }
-    });
-    observer.observe(link);
-  });
-
-  const fetchHTML = (href) => {
-    if (location.href !== href && location.pathname !== href) {
-      fetch(href)
-        .then((res) => res.text())
-        .then((html) => {
-          const doc = new DOMParser().parseFromString(html, "text/html");
-          document.documentElement.replaceWith(doc.documentElement);
-          Array.prototype.foreach.call(document.scripts, (script) => {
-            eval(script.innerHTML);
+  requestIdleCallback(
+    () => {
+      let timeoutIDArray = [];
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry, i) => {
+            entry.isIntersecting
+              ? (timeoutIDArray[i] = setTimeout(() => {
+                  createPrefetchLink(entry.target);
+                }, 500))
+              : clearTimeout(timeoutIDArray[i]);
           });
-          history.pushState({}, doc.title, href);
-        });
-    }
-  };
+        },
+        { threshold: 0.25 }
+      );
 
-  const createPrefetchLink = (element) => {
-    const { href } = element;
-    if (!document.querySelector('link[href="' + href + '"]')) {
-      const prefetchLink = document.createElement("link");
-      prefetchLink.href = href;
-      prefetchLink.rel = "prefetch";
-      document.head.appendChild(prefetchLink);
-    }
-  };
+      document
+        .querySelectorAll('a[href^="/":not([download])')
+        .forEach((link) => {
+          link.addEventListener("mouseover", () => {
+            createPrefetchLink(link);
+          });
+
+          link.addEventListener("click", (e) => {
+            if (!window.event.ctrlKey) {
+              e.preventDefault();
+              fetchHTML(link.href);
+            }
+          });
+          observer.observe(link);
+        });
+
+      const fetchHTML = (href) => {
+        if (location.href !== href && location.pathname !== href) {
+          fetch(href)
+            .then((res) => res.text())
+            .then((html) => {
+              const doc = new DOMParser().parseFromString(html, "text/html");
+              document.documentElement.replaceWith(doc.documentElement);
+              for (const script of document.scripts) {
+                eval(script.innerHTML);
+              }
+              history.pushState({}, doc.title, href);
+            });
+        }
+      };
+
+      const createPrefetchLink = (element) => {
+        const { href } = element;
+        if (!document.querySelector('link[href="' + href + '"]')) {
+          const prefetchLink = document.createElement("link");
+          prefetchLink.href = href;
+          prefetchLink.rel = "prefetch";
+          document.head.appendChild(prefetchLink);
+        }
+      };
+    },
+    { timeout: 2000 }
+  );
   return <h1>Hello World!</h1>;
 }
